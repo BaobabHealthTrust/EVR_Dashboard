8e11b4017c65eaca963bf4b1e21c87b4d71e6d68
phundi <kennethkapundi1@gmail.com>
2016-09-15 15:20:43 +0200

    APIs - 95% complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,36 +31,41 @@ class HomeController < ApplicationController
   def connection_lastseen_status
     data = YAML.load_file "#{Rails.root}/public/sites_data.yml"
     connections = {}
+
     data.each do |site, site_data|
-    next if site_data['ping_timestamp'].blank?
-    months_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60*24*30)).to_i
-    days_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60*24)).to_i
-    hrs_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60)).to_i
-    min_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60)).to_i
-    sec_diff = (Time.now - site_data['ping_timestamp'].to_time).to_i
 
-    if months_diff > 0
-        last_seen = "#{months_diff} months ago"
-    elsif days_diff > 0
-        last_seen = "#{days_diff} days ago"
-      elsif hrs_diff > 0
-        last_seen = "#{hrs_diff} hr ago"
-      elsif min_diff > 0
-        last_seen = "#{min_diff} min ago"
-      else
-        last_seen = "#{sec_diff} sec ago"
-      end
+      next if site_data['ping_timestamp'].blank?
+      months_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60*24*30)).to_i
+      days_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60*24)).to_i
+      hrs_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60*60)).to_i
+      min_diff = ((Time.now - site_data['ping_timestamp'].to_time)/(60)).to_i
+      sec_diff = (Time.now - site_data['ping_timestamp'].to_time).to_i
 
-      connections["#{site}"] = last_seen
+      if months_diff > 0
+          last_seen = "#{months_diff} months ago"
+      elsif days_diff > 0
+          last_seen = "#{days_diff} days ago"
+        elsif hrs_diff > 0
+          last_seen = "#{hrs_diff} hr ago"
+        elsif min_diff > 0
+          last_seen = "#{min_diff} min ago"
+        else
+          last_seen = "#{sec_diff} sec ago"
+        end
 
+        connections["#{site}"] = last_seen
     end
 
-    render :text => connections.to_json
+    render :text => [connections].to_json
   end
 
  def connection_lastseennews_status
     data = YAML.load_file "#{Rails.root}/public/sites_data.yml"
     connections = {}
+    births = {}
+    new_births = {}
+    deaths = {}
+    new_deaths = {}
     data.each do |site, site_data|
 		  next if site_data['news_ping_timestamp'].blank?
 			t = site_data['news_ping_timestamp']
@@ -78,10 +83,13 @@ class HomeController < ApplicationController
 			end
 
       connections["#{site}"] = index
-
+      births["#{site}"] = site_data['births']
+      new_births["#{site}"] = site_data['new_births']
+      deaths["#{site}"] = site_data['deaths']
+      new_deaths["#{site}"] = site_data['new_deaths']
     end
 
-    render :text => connections.to_json
+    render :text => [connections, births, new_births, deaths, new_deaths].to_json
   end
 		
 	def births_deaths_stats
--- lib/tasks/dashboard.rake
+++ lib/tasks/dashboard.rake
@@ -13,6 +13,10 @@ namespace :dashboard do
     ip = "#{ip}/api/dashboard"
     rst = JSON.parse(RestClient.get(ip)) rescue {}
     online = rst['online']
+    news_online = rst['news_online']
+    rbirths = rst['births'] || {}
+    rdeaths = rst['deaths'] || {}
+    rcounts = rst['counts'] || {}
 
     online.each do |site, lastseen|
       puts "EVR #{site}: #{lastseen}"
@@ -20,25 +24,40 @@ namespace :dashboard do
       result["#{site}"] = {} if result["#{site}"].blank?
       result["#{site}"]["ping"] = interval >= (Time.now - lastseen.to_time)
       result["#{site}"]["ping_timestamp"] = lastseen
-
-      births = rst["births"][site].to_i rescue 0
-      new_births = births - (result["#{site}"]["births"].to_i rescue 0)
-      result["#{site}"]["new_births"] = new_births
-      result["#{site}"]["births"] = births
-
-      deaths = rst["deaths"][site].to_i rescue 0
-      new_deaths = deaths - (result["#{site}"]["deaths"].to_i rescue 0)
-      result["#{site}"]["new_deaths"] = new_deaths
-      result["#{site}"]["deaths"] = deaths
     end
-	
-    news_online = rst['news_online']
+
+
     news_online.each do |site, lastseen|
       puts "EVR #{site}: #{lastseen}"
       result["#{site}"] = {} if result["#{site}"].blank?
 
-			result["#{site}"]["news_ping"] = interval >= (Time.now - lastseen.to_time)
+      result["#{site}"]["news_ping"] = interval >= (Time.now - lastseen.to_time)
       result["#{site}"]["news_ping_timestamp"] = lastseen
+    end
+
+
+    rbirths.each do |site, count|
+      result["#{site}"] = {} if result["#{site}"].blank?
+      births = count.to_i rescue 0
+      new_births = births - (result["#{site}"]["births"].to_i rescue 0)
+      result["#{site}"]["new_births"] = new_births
+      result["#{site}"]["births"] = births
+    end
+
+    rdeaths.each do |site, count|
+      result["#{site}"] = {} if result["#{site}"].blank?
+      deaths = count.to_i rescue 0
+      new_deaths = deaths - (result["#{site}"]["deaths"].to_i rescue 0)
+      result["#{site}"]["new_deaths"] = new_deaths
+      result["#{site}"]["deaths"] = deaths
+    end
+
+    rcounts.each do |site, count|
+      result["#{site}"] = {} if result["#{site}"].blank?
+      c = count.to_i rescue 0
+      new_c = c - (result["#{site}"]["count"].to_i rescue 0)
+      result["#{site}"]["new_count"] = new_c
+      result["#{site}"]["count"] = c
     end
 
     file = File.open("#{Rails.root}/public/sites_data.yml", "w")
